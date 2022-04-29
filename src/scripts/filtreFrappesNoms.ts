a4e7ee01665f1f63525e4b07853ff102e507e46d
Antoine Balaine <abalaineaccts@gmail.com>
2022-04-28 22:47:04 -0700

    Dictionnaire: mise pour filtreNoms

---
+++ src/scripts/filtreFrappesNoms.ts
@@ -0,0 +1,24 @@
+/*
+
+séparer les noms aux pluriel
+
+Préfixes:
+KElC quelques/quelque
+K que
+LA la
+L le/l'
+LE les
+S ce
+SA sa
+SE ses/ces
+S ce
+SOn son
+ST cette/cet
+T*E des
+Un un
+UnC une
+
+Sépare les mots au pluriel de ceux au singulier
+Sépare les noms masculins des féminins
+vire les préfixes
+*/
